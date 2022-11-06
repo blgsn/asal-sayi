4f056a7fbb8c464f2f0b68ae4de836dfeb7912e6
blgsn <111683241+blgsn@users.noreply.github.com>
2022-11-07 01:37:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,18 @@
 
-import java.util.Scanner;
+iimport java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		
-		
-     for(int i=2;i<=100;i++) {
+	
+     for(int i=2;i<=100;i++) {//1 is not prime number so Numbers from 2 to 100.
     	 int flag=0;
     	 for(int j=2;j<i;j++) {
-    		 if (i%j==0) {
+    		 if (i%j==0) {//Numbers with divisors other than themselves and 1 are not prime numbers.
     			 flag=1;
     				 break;
     				 }}
     		 if(flag==0) {
         	 
-        	 System.out.print(i+" ");
-        	
-        	 
+        	 System.out.print(i+" ");//prints prime numbers
     				 }
      }
 	 } 
